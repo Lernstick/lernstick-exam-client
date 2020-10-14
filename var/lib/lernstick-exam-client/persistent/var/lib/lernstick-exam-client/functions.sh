0d6efc43d65972f9edf45ad2304c562f16cf31ab
chaoos <p1020389@yahoo.com>
2020-10-14 15:44:23 +0200

    fixed functions.sh

--- var/lib/lernstick-exam-client/persistent/var/lib/lernstick-exam-client/functions.sh
+++ var/lib/lernstick-exam-client/persistent/var/lib/lernstick-exam-client/functions.sh
@@ -2,18 +2,15 @@
 
 infoFile="/info"
 configFile="/config.json"
-python="/usr/bin/python"
 DEBUG=true
 
 [ -r "${infoFile}" ] && . ${infoFile}
 
 function config_value()
 {
-  if [ -n "${config}" ]; then
-    config="$(cat "${configFile}")"
-  fi
-
-  v="$(echo "${config}" | ${python} -c 'import sys, json; print json.load(sys.stdin)["config"]["'${1}'"]')"
+  config="$(cat "${configFile}")"
+  v="$(cat "${configFile}" | python2 -c 'import sys, json; print json.load(sys.stdin)["config"]["'${1}'"]')"
   $DEBUG && >&2 echo "${1} is set to ${v}"
   echo "$v"
 }
+
