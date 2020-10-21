b438c78e8fa19eef9b0897bc208813de9ed65ca9
root <root@localhost.localdomain>
2020-10-21 10:58:41 +0200

    max_brightness script added

--- var/lib/lernstick-exam-client/persistent/var/lib/lernstick-exam-client/functions.sh
+++ var/lib/lernstick-exam-client/persistent/var/lib/lernstick-exam-client/functions.sh
@@ -3,14 +3,23 @@
 infoFile="/info"
 configFile="/config.json"
 DEBUG=true
+python="python2"
 
 [ -r "${infoFile}" ] && . ${infoFile}
 
+# Echo the configuration value from the config json
+# @param $1 the config value to return
+# @param $2 the default value if the config value is not set
 function config_value()
 {
-  config="$(cat "${configFile}")"
-  v="$(cat "${configFile}" | python2 -c 'import sys, json; print json.load(sys.stdin)["config"]["'${1}'"]')"
-  $DEBUG && >&2 echo "${1} is set to ${v}"
-  echo "$v"
+  v="$(cat "${configFile}" | ${python} -c 'import sys, json; print json.load(sys.stdin)["config"]["'${1}'"]' 2>/dev/null)"
+  retval=$?
+  if [ ${retval} -ne 0 ]; then
+    $DEBUG && >&2 echo "${1} not found in config file"
+    echo "${2}" #return default value
+  else
+    $DEBUG && >&2 echo "${1} is set to ${v}"
+    echo "$v"
+  fi
 }
 
